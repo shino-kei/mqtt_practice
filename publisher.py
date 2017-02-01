c0ebf8ed86ccc2103489ac420dc8677d1af3e834
Keisuke <shino_kei@asagi.waseda.jp>
2017-02-01 12:44:55 +0900

    Subscriberのサンプルを修正

--- publisher.py
+++ publisher.py
@@ -5,7 +5,8 @@ from time import sleep
 import paho.mqtt.client as mqtt
 
 # MQTTブローカーのアドレスとポートを指定する
-host = '192.168.11.11'
+# host = '192.168.11.11'
+host = '127.0.0.1'
 port = 1883
 
 # インスタンスの生成
--- subscriber.py
+++ subscriber.py
@@ -4,21 +4,34 @@
 
 import paho.mqtt.client as mqtt
 
+# ブローカーの設定
 host = '127.0.0.1'
 port = 1883
-topic = 'test'
-topic2 = 'test2'
+
+FORWARD_TOPIC = 'ev3/forward'
+BACK_TOPIC = 'ev3/back'
+
 def on_connect(client, userdata, flags, respons_code):
     print('status {0}'.format(respons_code))
 
-    client.subscribe(topic) # topic名を指定
-    client.subscribe(topic2) # topic名を指定
+    client.subscribe(FORWARD_TOPIC) # topic名を指定
+    client.subscribe(BACK_TOPIC) # topic名を指定
 
 def on_message(client, userdata, msg):
     print(msg.topic + ' ' + str(msg.payload))
 
-def on_topic2_received(client, userdata, msg):
-    print("topic2 Reseived !! : "+ str(msg.payload))
+def on_foward(client, userdata, msg):
+    print("foward: " + str(msg.payload))
+
+    # ここにforwardトピックを受け取った時の処理を書く
+
+def on_back(client, userdata, msg):
+    print("back : " + str(msg.payload))
+
+    # ここにbackトピックを受け取ったときの処理を書く
+
+
+
 
 
 if __name__ == '__main__':
@@ -27,10 +40,15 @@ if __name__ == '__main__':
         # mqttクライアントの生成
         client = mqtt.Client()
 
-        # コールバックを登録する
+        ## コールバックを登録する
+        # 接続時に実行される処理
         client.on_connect = on_connect
+        # 何かしらのメッセージを受け取ったとき
         client.on_message = on_message
-        client.message_callback_add(topic2, on_topic2_received)
+
+        # 各トピックに対するコールバック
+        client.message_callback_add(FORWARD_TOPIC, on_foward)
+        client.message_callback_add(BACK_TOPIC, on_back)
 
         client.connect(host, port=port, keepalive=60)
 
